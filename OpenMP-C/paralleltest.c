4c7df5a6030d7e44017a465d3d4a573f58360adf
Krüpl Zsolt <hg2ecz@ham.hu>
2017-03-14 09:21:23 +0100

    +OpenMP-C

---
+++ OpenMP-C/paralleltest.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define FALSE 0
+#define TRUE !FALSE
+
+int main() {
+    double eltime;
+    struct timespec gstart, gend;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &gstart);
+
+    int primct=0;
+
+#if defined(_OPENMP)
+    #pragma omp parallel for
+#endif
+
+    for (int i=MAXNUM; i>2; i--) {
+	_Bool prim = TRUE;
+	int sqi = sqrt(i);
+	for (int j = 2; j<=sqi; j++) {
+	    if (!(i%j)) {
+		prim=FALSE;
+		break;
+	    }
+	}
+	if (prim == TRUE) primct++;
+    }
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &gend);
+    eltime = 1000.0*(gend.tv_sec - gstart.tv_sec) + (gend.tv_nsec - gstart.tv_nsec)/1000000.;
+    printf("%d %.3f ms\n", primct, eltime);
+}
