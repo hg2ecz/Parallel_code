26e9acc2f1c931e78a32cb59e63b9d5ccb62cb7e
Robert FUSZENECKER <fuszenecker@users.noreply.github.com>
2017-03-16 21:26:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-cd C#-Linq && dotnet restore && dotnet build -c Release && dotnet test -c Release
+cd C#-Linq && dotnet restore && dotnet build -c Release
+cd C# -Linq/ParallelTest.UnitTests && dotnet restore && dotnet test -c Release
